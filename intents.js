5e5a39bf26162ba84eec5dc5817dee1eadb3946e
Domingo <domingohui@gmail.com>
2017-01-08 01:44:28 -0500

    use AMAZON built-in intents for help and stop

--- intents.js
+++ intents.js
@@ -1,30 +1,43 @@
-module.exports = function(app) {
-    app.intent("start", {
-        "slots": { "number": "NUMBER" },
-        "utterances": ["say the number {1-5|number}"]
-    },
-        function(request, response) {
-            var number = request.slot("number");
-            response.say("You asked for the number " + number);
-            response.shouldEndSession(false);
-        }
-    );
+module.exports = function(app)
+{
+    app.intent("start",
+            {
+                "slots": { "number": "NUMBER" },
+                "utterances": ["say the number {1-5|number}"]
+            },
+            function(request, response) {
+                var number = request.slot("number");
+                response.say("You asked for the number " + number);
+                response.shouldEndSession(false);
+            }
+            );
 
-    app.intent("end", {
-        "slots": { },
-        "utterances": ["{Goodbye|Thanks} Alexa {|I feel much better now}"]
-    },
-        function(request, response) {
-            console.log ( "SessionEnd Intent." );
-            response.say ( "Have a nice day!" );
-            response.shouldEndSession(true);
-        }
-    );
-    
-    app.intent("freeForm",function(request, response) {
-            console.log ( "Parsing freefrom input" );
-            response.say ( "I see. Please tell me more" );
-            response.shouldEndSession(false);
-        }
-    );
+    app.intent("end", 
+            {
+                "slots": { },
+                "utterances": ["{Goodbye|Thanks} Alexa {|I feel much better now}"]
+            },
+            function(request, response) {
+                console.log ( "SessionEnd Intent." );
+                response.say ( "Have a nice day!" );
+                response.shouldEndSession(true);
+            }
+            );
+
+    app.intent("freeForm", parseHelpWrapper);
+    app.intent("AMAZON.HelpIntent", parseHelpWrapper);
+
+    app.intent ( "AMAZON.StopIntent", function ( request, response )
+            {
+                console.log ( "Stop intent" );
+                response.say ( "Ok. Take care. I will go now" );
+            }
+            );
+}
+
+
+function parseHelpWrapper (request, response) {
+    console.log ( "Parsing freefrom input" );
+    response.say ( "I see. Please tell me more" );
+    response.shouldEndSession(false);
 }
